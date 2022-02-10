ff52dde9c89a5333fadc500723ffe835b3c3580d
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-10 11:17:37 +0530

    working on exchange rates

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,22 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
-import 'package:upcloud_task1/settings.dart';
 
 Future<void> main() async {
   Response res = await http.get(Uri.parse(
       "https://rest.coinapi.io/v1/exchanges/?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"));
   List<dynamic> Data = jsonDecode(res.body);
-  // print(data[0]['name']);
+  List<dynamic> Rates = [0];
+  for (var i = 0; i < Data.length; i++) {
+    Response res2 = await http.get(Uri.parse(
+        "https://rest.coinapi.io/v1/exchanges/" +
+            Data[i]["exchange_id"] +
+            "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"));
+    List<dynamic> Data2 = jsonDecode(res2.body);
+    Rates.add(Data2[0]['rate']);
+  }
+  print(Rates);
+
   runApp(MyApp(data: Data));
 }
 
@@ -45,47 +54,60 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           actions: <Widget>[
             Padding(
-              padding: EdgeInsets.only(right: 20.0),
+              padding: const EdgeInsets.only(right: 20.0),
               child: IconButton(
                 icon: const Icon(
                   Icons.settings,
                 ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => settingspage()));
-                },
+                onPressed: () {},
               ),
             ),
           ],
-          title: Text('Crypto App'),
+          title: const Text('Crypto App'),
         ),
         body: Center(
-          child: Container(
-              child: ListView.builder(
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(data[index]["exchange_id"],
-                    style: const TextStyle(fontWeight: FontWeight.bold)),
-                subtitle: Text(
-                  data[index]['name'] + "\n" + data[index]['website'],
-                ),
-                isThreeLine: true,
-                leading: CircleAvatar(
-                  backgroundColor: primaries[index % primaries.length],
-                  child: Text(
-                    data[index]["name"][0],
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              );
-            },
-            itemCount: data.length,
-          )),
+          child: _customUIwidget(data, primaries),
         ),
       ),
     );
   }
+}
+
+getData(List<dynamic> data, int index) {
+  Response res2 = http.get(Uri.parse("https://rest.coinapi.io/v1/exchanges/" +
+      data[index]["exchange_id"] +
+      "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C")) as http.Response;
+  List<dynamic> Data2 = jsonDecode(res2.body);
+}
+
+Widget _customUIwidget(List<dynamic> data, List<MaterialColor> primaries) {
+  return ListView.builder(
+    itemBuilder: (context, index) {
+      // Response res2 = http.get(Uri.parse(
+      //         "https://rest.coinapi.io/v1/exchanges/" +
+      //             data[index]["exchange_id"] +
+      //             "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"))
+      //     as http.Response;
+      // List<dynamic> Data2 = jsonDecode(res2.body);
+      return ListTile(
+        title: Text(data[index]["exchange_id"],
+            style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(
+          data[index]['name'] + "\n" + "\$ ",
+        ),
+        isThreeLine: true,
+        leading: CircleAvatar(
+          backgroundColor: primaries[index % primaries.length],
+          child: Text(
+            data[index]["name"][0],
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+    },
+    itemCount: data.length,
+  );
 }
