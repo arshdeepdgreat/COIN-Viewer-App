d50c22152e447ad1cdb9d51ff1f31f5a8518b671
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-10 11:51:14 +0530

    working on exchange rates

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,29 @@ import 'package:http/http.dart';
 
 Future<void> main() async {
   Response res = await http.get(Uri.parse(
-      "https://rest.coinapi.io/v1/exchanges/?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"));
+      "https://rest.coinapi.io/v1/assets/?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"));
   List<dynamic> Data = jsonDecode(res.body);
   List<dynamic> Rates = [0];
-  for (var i = 0; i < Data.length; i++) {
+  for (var i = 0; i < 10; i++) {
     Response res2 = await http.get(Uri.parse(
-        "https://rest.coinapi.io/v1/exchanges/" +
-            Data[i]["exchange_id"] +
+        "https://rest.coinapi.io/v1/exchangerate/" +
+            Data[i]["asset_id"] +
             "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C"));
-    List<dynamic> Data2 = jsonDecode(res2.body);
-    Rates.add(Data2[0]['rate']);
+    //  print("https://rest.coinapi.io/v1/exchangerate/" +
+    //     Data[i]["asset_id"] +
+    //     "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C");
+    if (res2.body.isNotEmpty) {
+      // List<dynamic> Data2 = jsonDecode(res2.body);
+      Rates.add(jsonDecode(res2.body));
+    }
   }
-  print(Rates);
+  // print(Rates);
 
-  runApp(MyApp(data: Data));
+  runApp(MyApp(data: Data, rate: Rates));
 }
 
 class MyApp extends StatelessWidget {
-  final List<dynamic> data;
+  final List<dynamic> data, rate;
   List<MaterialColor> primaries = <MaterialColor>[
     Colors.red,
     Colors.pink,
@@ -41,7 +46,7 @@ class MyApp extends StatelessWidget {
     Colors.amber,
     Colors.orange
   ];
-  MyApp({Key? key, required this.data}) : super(key: key);
+  MyApp({Key? key, required this.data, required this.rate}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -66,21 +71,22 @@ class MyApp extends StatelessWidget {
           title: const Text('Crypto App'),
         ),
         body: Center(
-          child: _customUIwidget(data, primaries),
+          child: _customUIwidget(data, primaries, rate),
         ),
       ),
     );
   }
 }
 
-getData(List<dynamic> data, int index) {
-  Response res2 = http.get(Uri.parse("https://rest.coinapi.io/v1/exchanges/" +
-      data[index]["exchange_id"] +
-      "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C")) as http.Response;
-  List<dynamic> Data2 = jsonDecode(res2.body);
-}
+// getData(List<dynamic> data, int index, List<dynamic> rate) {
+//   Response res2 = http.get(Uri.parse("https://rest.coinapi.io/v1/exchanges/" +
+//       data[index]["exchange_id"] +
+//       "/USD?apikey=3FA1E567-4C1F-4D94-A36F-6B8E326EC75C")) as http.Response;
+//   List<dynamic> Data2 = jsonDecode(res2.body);
+// }
 
-Widget _customUIwidget(List<dynamic> data, List<MaterialColor> primaries) {
+Widget _customUIwidget(
+    List<dynamic> data, List<MaterialColor> primaries, rate) {
   return ListView.builder(
     itemBuilder: (context, index) {
       // Response res2 = http.get(Uri.parse(
@@ -90,10 +96,13 @@ Widget _customUIwidget(List<dynamic> data, List<MaterialColor> primaries) {
       //     as http.Response;
       // List<dynamic> Data2 = jsonDecode(res2.body);
       return ListTile(
-        title: Text(data[index]["exchange_id"],
+        title: Text(data[index]["asset_id"],
             style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(
-          data[index]['name'] + "\n" + "\$ ",
+          data[index]['name'] +
+              "\n" +
+              "\$" +
+              rate[index + 1]['rate'].toString(),
         ),
         isThreeLine: true,
         leading: CircleAvatar(
@@ -108,6 +117,6 @@ Widget _customUIwidget(List<dynamic> data, List<MaterialColor> primaries) {
         ),
       );
     },
-    itemCount: data.length,
+    itemCount: 10,
   );
 }
